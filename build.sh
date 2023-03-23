1149731626bf201ecb60ebffdfe12a4b7de54d69
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-03-23 19:02:14 +0530

    try to build alpha

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BiancaProject/android.git -b 13-rebase-android-13.0.0_r35 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13-bliss device/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13.0-alphadroid device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
 
@@ -11,7 +11,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device rova --lunch bliss_rova-userdebug
-./barom.sh -b -j 8 -u wet --timer 95m -- m blissify rova
+./barom.sh --device rova --lunch lineage_rova-userdebug
+./barom.sh -b -j 8 -u wet --timer 95m -- mka bacon
 
 # end
