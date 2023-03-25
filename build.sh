1dee70e8bdd63eb970f2b2943e7c2ddf567e582c
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-03-25 16:19:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git 
 git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
 
-
 # build rom
 curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
