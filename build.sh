55e66f5b7241d6e3d27916df38360c4b73eb02ff
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-03-30 13:51:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Maxx12211/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13-banana device/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13-cherish device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
 
@@ -10,7 +10,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device rova --lunch banana_rova-userdebug
-./barom.sh -b -j 8 -u wet --timer 110m -- m banana
+./barom.sh --device rova --lunch cherish_rova-userdebug
+./barom.sh -b -j 8 -u wet --timer 95m -- mka bacon
 
-# end 
+# end
