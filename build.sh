9cb516a1db8c7dfcc74aa9e386fbf7599904bf31
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-06 01:00:39 +0530

    new build

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Maxx12211/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Maxx12211/android_manifest -b mi8937 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Maxx12211/local_manifests-rdp.git --depth 1 -b a13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13-cherish device/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13 vendor/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
 
 # build rom
 curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device rova --lunch cherish_rova-userdebug
+./barom.sh --device Mi8937 --lunch cherish_Mi8937-userdebug
 ./barom.sh -b -j 8 -u gof --timer 95m -- mka bacon
 
 # end
