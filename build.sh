2f9d4f13b085247078fa02745d457072fc9e2000
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-09 11:52:55 +0530

    update device

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device Mi8937 --lunch lineage_Mi8937-userdebug
+./barom.sh --device rova --lunch lineage_rova-userdebug
 ./barom.sh -b -j 8 -u gof --timer 95m -- mka bacon
 
 # end
