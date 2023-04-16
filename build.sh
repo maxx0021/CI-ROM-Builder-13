3782deb4f615fe6415bc9ef01e8760d2fa38a642
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-16 15:12:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/maxx0021/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
@@ -7,18 +7,17 @@ repo init --git-lfs
 rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
 rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-git clone https://github.com/maxx0021/local_manifests-rdp -b lineage .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-#git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13-cherish device/xiaomi/rova
-#git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13 vendor/xiaomi/rova
-#git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
+repo sync -j16
+git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b alpha-13 device/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b alpha-13 vendor/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b alpha-13 kernel/xiaomi/rova
 
 # build rom
 curl -Lo barom.sh https://raw.githubusercontent.com/Maxx12211/barom/main2/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device Mi8937 --lunch lineage_Mi8937-userdebug
+./barom.sh --device rova --lunch lineage_rova-userdebug
 ./barom.sh -b -j 8 -u gof --timer 105m -- mka bacon
 
 # end
