7114b595cf3691f029d81d4fce3dd9a78cc3e93d
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-18 16:17:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-#sudo apt-get install git-lfs
-#repo init --git-lfs
-#rm -rf external/chromium-webview/prebuilt/*
-#rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-#rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-#repo sync -j16
-git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-blaze device/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-cherish device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/vendor_xiaomi_rova.git -b a13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/kernel_xiaomi_rova.git -b a13 kernel/xiaomi/rova
 
@@ -17,7 +10,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/Maxx12211/barom/main2/barom.
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device rova --lunch blaze_rova-userdebug
+./barom.sh --device rova --lunch cherish_rova-userdebug
 ./barom.sh -b -j 8 -u gof --timer 95m -- mka bacon
 
 # end
