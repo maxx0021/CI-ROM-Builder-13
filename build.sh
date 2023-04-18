ce10078bdb5a10255495838d4ed17397be71394c
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-18 16:38:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_man
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-cherish device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/vendor_xiaomi_rova.git -b a13 vendor/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/kernel_xiaomi_rova.git -b a13 kernel/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/kernel_xiaomi_msm8937.git -b a13 kernel/xiaomi/rova
 
 # build rom
 curl -Lo barom.sh https://raw.githubusercontent.com/Maxx12211/barom/main2/barom.sh
