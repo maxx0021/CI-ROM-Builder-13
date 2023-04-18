c745b6e4ed56652815119fea840b4e79126978b8
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-18 14:19:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
-repo init --git-lfs
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b tmp/alpha device/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b alpha-13 vendor/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b alpha-13 kernel/xiaomi/rova
+#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+#sudo apt-get install git-lfs
+#repo init --git-lfs
+#rm -rf external/chromium-webview/prebuilt/*
+#rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+#rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+#repo sync -j16
+git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-blaze device/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/vendor_xiaomi_rova.git -b a13 vendor/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/kernel_xiaomi_rova.git -b a13 kernel/xiaomi/rova
 
 # build rom
 curl -Lo barom.sh https://raw.githubusercontent.com/Maxx12211/barom/main2/barom.sh
