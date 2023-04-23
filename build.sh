59054ce1d59f09ccb4df6d6ecf6baab556ad3b5b
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-23 21:28:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android.git -b thirteen -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-cherish device/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-risingos device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/vendor_xiaomi_rova.git -b a13 vendor/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/kernel_xiaomi_msm8937.git -b a13 kernel/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_msm8937.git -b a13/master kernel/xiaomi/msm8937
 
 # build rom
+mv hardware/lineage/compat/Android.bp hardware/lineage/compat/Android.bpp
 curl -Lo barom.sh https://raw.githubusercontent.com/Maxx12211/barom/main2/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
