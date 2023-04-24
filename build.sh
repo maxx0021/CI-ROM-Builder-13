7630a8145668f5f741da845bd27e23d1b6d3dde6
maxx0021 <100127589+maxx0021@users.noreply.github.com>
2023-04-24 23:10:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android.git -b thirteen -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+repo sync
 git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-risingos device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/vendor_xiaomi_rova.git -b a13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_msm8937.git -b a13/master kernel/xiaomi/msm8937
@@ -12,6 +12,6 @@ chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
 ./barom.sh --device rova --lunch lineage_rova-userdebug
-./barom.sh -b -j 8 -u gof --timer 95m -- mka bacon
+./barom.sh -b -j 8 -u gof --timer 60m -- mka bacon
 
 # end
